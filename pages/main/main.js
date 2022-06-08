63a58363b5d76f56c18f5a473796826853568008
Soso Beriashvili <sosiberiashvili@gmail.com>
2022-06-08 13:47:33 +0400

    feat: complete refactoring code

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,152 +1,3 @@
-// if(document.readyState == 'loading'){
-//   document.addEventListener('DOMContentLoaded', ready);
-// } else {
-//   ready();
-// }
-
-// function ready(){
-//   const petsSection = document.querySelector('.pets-selection');
-//   const petsMain = document.querySelector('.pets-grid');
-
-// const pets = [
-//   {
-//     "name": "Jennifer",
-//     "img": "../../assets/images/pets-jennifer.svg",
-//     "type": "Dog",
-//     "breed": "Labrador",
-//     "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
-//     "age": "2 months",
-//     "inoculations": ["none"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Sophia",
-//     "img": "../../assets/images/pets-sophia.svg",
-//     "type": "Dog",
-//     "breed": "Shih tzu",
-//     "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
-//     "age": "1 month",
-//     "inoculations": ["parvovirus"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Woody",
-//     "img": "../../assets/images/pets-woody.svg",
-//     "type": "Dog",
-//     "breed": "Golden Retriever",
-//     "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
-//     "age": "3 years 6 months",
-//     "inoculations": ["adenovirus", "distemper"],
-//     "diseases": ["right back leg mobility reduced"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Scarlett",
-//     "img": "../../assets/images/pets-scarlet.svg",
-//     "type": "Dog",
-//     "breed": "Jack Russell Terrier",
-//     "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
-//     "age": "3 months",
-//     "inoculations": ["parainfluenza"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Katrine",
-//     "img": "../../assets/images/pets-katrine.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
-//     "age": "6 months",
-//     "inoculations": ["panleukopenia"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Timmy",
-//     "img": "../../assets/images/pets-timmy.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
-//     "age": "2 years 3 months",
-//     "inoculations": ["calicivirus", "viral rhinotracheitis"],
-//     "diseases": ["kidney stones"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Freddie",
-//     "img": "../../assets/images/pets-freddie.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
-//     "age": "2 months",
-//     "inoculations": ["rabies"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Charly",
-//     "img": "../../assets/images/pets-charly.svg",
-//     "type": "Dog",
-//     "breed": "Jack Russell Terrier",
-//     "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
-//     "age": "8 years",
-//     "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
-//     "diseases": ["deafness", "blindness"],
-//     "parasites": ["lice", "fleas"]
-//   }
-// ];
-
-// pets.forEach(function(pet){
-//   const {name, img, type, breed, desc, age, inoc, diseases, paraites} = pet;
-//   const petDiv = `<div class="pets-content" id="${name.toLowerCase()}">
-//     <img class="margin-bottom--3rem" src="${img}" alt="${name} photo">
-//      <p class="pet-names margin-bottom--4rem">${name}</p>
-//        <button class="btn btn-pets">Learn More</button>
-//   </div>`
-  
-//   petsMain.insertAdjacentHTML('afterbegin', petDiv);
-//   const petsContent = document.querySelector('.pets-content');
-  
-//   petsContent.style.position = 'absolute';   
-//   petsMain.style.position = 'relative';
-// });
-
-// const btnRight = document.querySelector('.btn-right');
-
-// let curSlide = 0;
-
-// // slides.forEach((s, i) => {
-// //   s.style.transform = `translateX(${100 * i}%)`;
-// // })
-
-// btnRight.addEventListener('click', function(){
-//   const slides = document.querySelectorAll('.pets-content');
-//   const slidesArray = Array.from(slides);
-  
-  
-//   let randomCard = Math.floor(Math.random() * slidesArray.length);
-//   if(curSlide === slidesArray.length - 1){
-//     curSlide = 0;
-//   } else {
-//     curSlide++;
-//     // console.log(curSlide);
-//   }
-//   // console.log(slides[randomCard]);
-//   // console.log(Array.from(slides));
-  
-//   // let randomCard = Math.floor(Math.random() * slides.length);
-//   // console.log(slides)
-//   slidesArray.forEach((s, i) => {
-//     s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-//     console.log(i)
-//   })
-//   // console.log(randomCard)
-// })
-
-
 // HAMBURGER MENU
 
 const hamburger = document.querySelector('.hamburger');
@@ -267,12 +118,13 @@ const pets = [
 const petsGrid = document.querySelector('.pets-grid');
 const btnRight = document.querySelector('.btn-right');
 const btnLeft = document.querySelector('.btn-left');
+const popUp = document.querySelector('.pop-up');
+const popUpDiv = document.querySelector('.pop-up-data');
 
 
-let current_page = 0;
-let rows = screen.width >= 1280 ? 3 : screen.width >= 768 ? 2 : 1; 
+let rows = screen.width >= 1280 ? 3 : screen.width >= 768 ? 2 : 1;
 
-
+let paginatedItems;
 
 function DisplayList(items, wrapper){
   wrapper.innerHTML = '';
@@ -280,11 +132,9 @@ function DisplayList(items, wrapper){
 
     for(let i = 0; i < rows; i++){
       let pet = pets[i];
+      const {name, img} = pet;
 
-
-      const {name, img, type, breed, desc, age, inoc, diseases, paraites} = pet;
-
-      const petDiv = `<div class="pets-content" id="${name.toLowerCase()}">
+      const petDiv = `<div class="pets-content" id="${name}">
       <img class="margin-bottom--3rem" src="${img}" alt="${name} photo">
       <p class="pet-names margin-bottom--4rem">${name}</p>
         <button class="btn btn-pets">Learn More</button>
@@ -293,42 +143,50 @@ function DisplayList(items, wrapper){
       wrapper.insertAdjacentHTML('afterbegin', petDiv);
     }
 
+    const petContent = document.querySelectorAll('.pets-content');
+    console.log(petContent)
 
-  // let start = rows_per_page * page;
-  // let end = start + rows_per_page;
-  // paginatedItems = items.slice(start, end);
+    for(let i = 0; i < rows; i++){      
+      petContent[i].addEventListener('click', function(e){
+        popUpDiv.innerHTML = '';
 
-  // for(let i = 0; i < paginatedItems.length; i++){
-  //   let currentPet = Math.floor(Math.random() * items.length);
+        const trigerredPet = pets.findIndex(obj => obj.name === petContent[i].id);
 
-  //   console.log(currentPet)
+        const {name, img, type, breed, description, age, inoculations, diseases, parasites} = pets[trigerredPet];
 
+        const popUpContent = `
+        <button><strong>&#10006;</strong></button>
+        <div class="img-more-info">
+          <img src="${img}" alt="Woody">
+        </div>
+        <div class="more-info-container">
+          <h3>${name}</h3>
+          <h4>${breed}</h4>
+          <p>${description}</p>
+          <ul>
+            <li><strong>Age:</strong> ${age}</li>
+            <li><strong>Inoculations:</strong> ${inoculations.join(', ')}</li>
+            <li><strong>Diseases:</strong> ${diseases.join(', ')}</li>
+            <li><strong>Parasites:</strong> ${parasites.join(', ')}</li>
+          </ul>
+        </div>`
 
-  //   let pet = items[currentPet];
+      popUpDiv.insertAdjacentHTML('afterbegin', popUpContent);
 
-  //   const {name, img, type, breed, desc, age, inoc, diseases, paraites} = pet;
-
-  //   const petDiv = `<div class="pets-content" id="${name.toLowerCase()}">
-  //     <img class="margin-bottom--3rem" src="${img}" alt="${name} photo">
-  //     <p class="pet-names margin-bottom--4rem">${name}</p>
-  //       <button class="btn btn-pets">Learn More</button>
-  //   </div>`
-
-  //    wrapper.insertAdjacentHTML('afterbegin', petDiv);
-  //   }
-
-  //   const petContent = document.querySelectorAll('.pets-content');
-
-  //   for(let i = 0; i < paginatedItems.length; i++){
-  //     petContent[i].addEventListener('click', function(){
-  //     popUp.style.display = 'flex';
-  //     popUpDiv.style.display = 'block';
-  //     document.body.style.overflowY = 'hidden';
-    
-  //     console.log('clicked');
-  //     })
-  //   }
+      popUp.style.display = 'flex';
+      popUpDiv.style.display = 'flex';
+      document.body.style.overflowY = 'hidden';
+      document.querySelector('.more-info-container').style.width = '100%';    
+      })
+    }
   }
+
+popUp.addEventListener('click', function(e){
+  if(!e.target.classList.contains('pop-up-data')){
+    popUp.style.display = 'none';
+    document.body.style.overflowY = 'visible';
+  }
+})
 
 
 DisplayList(pets, petsGrid);
@@ -340,14 +198,3 @@ btnRight.addEventListener('click', () => {
 btnLeft.addEventListener('click', () => {
   DisplayList(pets, petsGrid);
 });
-
-// @keyframes append-animate {
-// 	from {
-// 		transform: scaleY(0);
-// 		opacity: 0;
-// 	}
-// 	to {
-// 		transform: scaleY(1);
-// 		opacity: 1;
-// 	}
-// }
--- pages/pets/pets.js
+++ pages/pets/pets.js
@@ -529,96 +529,6 @@ const pets = [
   }  
 ];
 
-// const pets = [
-//   {
-//     "name": "Jennifer",
-//     "img": "../../assets/images/pets-jennifer.svg",
-//     "type": "Dog",
-//     "breed": "Labrador",
-//     "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
-//     "age": "2 months",
-//     "inoculations": ["none"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Sophia",
-//     "img": "../../assets/images/pets-sophia.svg",
-//     "type": "Dog",
-//     "breed": "Shih tzu",
-//     "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
-//     "age": "1 month",
-//     "inoculations": ["parvovirus"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Woody",
-//     "img": "../../assets/images/pets-woody.svg",
-//     "type": "Dog",
-//     "breed": "Golden Retriever",
-//     "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
-//     "age": "3 years 6 months",
-//     "inoculations": ["adenovirus", "distemper"],
-//     "diseases": ["right back leg mobility reduced"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Scarlett",
-//     "img": "../../assets/images/pets-scarlet.svg",
-//     "type": "Dog",
-//     "breed": "Jack Russell Terrier",
-//     "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
-//     "age": "3 months",
-//     "inoculations": ["parainfluenza"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Katrine",
-//     "img": "../../assets/images/pets-katrine.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
-//     "age": "6 months",
-//     "inoculations": ["panleukopenia"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Timmy",
-//     "img": "../../assets/images/pets-timmy.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
-//     "age": "2 years 3 months",
-//     "inoculations": ["calicivirus", "viral rhinotracheitis"],
-//     "diseases": ["kidney stones"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Freddie",
-//     "img": "../../assets/images/pets-freddie.svg",
-//     "type": "Cat",
-//     "breed": "British Shorthair",
-//     "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
-//     "age": "2 months",
-//     "inoculations": ["rabies"],
-//     "diseases": ["none"],
-//     "parasites": ["none"]
-//   },
-//   {
-//     "name": "Charly",
-//     "img": "../../assets/images/pets-charly.svg",
-//     "type": "Dog",
-//     "breed": "Jack Russell Terrier",
-//     "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
-//     "age": "8 years",
-//     "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
-//     "diseases": ["deafness", "blindness"],
-//     "parasites": ["lice", "fleas"]
-//   }
-// ];
 
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.main-nav-list');
@@ -674,7 +584,6 @@ function DisplayList(items, wrapper, rows_per_page, page){
   
   for(let i = 0; i < paginatedItems.length; i++){
     let pet = randomArr[i];
-
     const {name, img} = pet;
 
     const petDiv = `<div class="pets-content" id="${name}">
@@ -683,21 +592,17 @@ function DisplayList(items, wrapper, rows_per_page, page){
         <button class="btn btn-pets">Learn More</button>
     </div>`
 
-     wrapper.insertAdjacentHTML('afterbegin', petDiv);
-    
+     wrapper.insertAdjacentHTML('afterbegin', petDiv);    
     }
 
     const petContent = document.querySelectorAll('.pets-content');
 
-    for(let i = 0; i < paginatedItems.length; i++){
-      
-      
+    for(let i = 0; i < paginatedItems.length; i++){      
       petContent[i].addEventListener('click', function(e){
         popUpDiv.innerHTML = '';
         const trigerredPet = paginatedItems.findIndex(obj => obj.name === petContent[i].id);
 
-        const {name, img, type, breed, description, age, inoc, diseases, paraites} = paginatedItems[trigerredPet];
-
+        const {name, img, type, breed, description, age, inoculations, diseases, parasites} = paginatedItems[trigerredPet];
 
         const popUpContent = `
         <button><strong>&#10006;</strong></button>
@@ -710,26 +615,21 @@ function DisplayList(items, wrapper, rows_per_page, page){
           <p>${description}</p>
           <ul>
             <li><strong>Age:</strong> ${age}</li>
-            <li><strong>Inoculations:</strong> ${inoc}</li>
-            <li><strong>Diseases:</strong> ${diseases}</li>
-            <li><strong>Parasites:</strong>${paraites}</li>
+            <li><strong>Inoculations:</strong> ${inoculations.join(', ')}</li>
+            <li><strong>Diseases:</strong> ${diseases.join(', ')}</li>
+            <li><strong>Parasites:</strong> ${parasites.join(', ')}</li>
           </ul>
         </div>`
-
-
 
       popUpDiv.insertAdjacentHTML('afterbegin', popUpContent);
 
       popUp.style.display = 'flex';
       popUpDiv.style.display = 'flex';
       document.body.style.overflowY = 'hidden';
-    
-      console.log('clicked');
-    
+      document.querySelector('.more-info-container').style.width = '100%';    
       })
     }
   }
-
 
 DisplayList(pets, list_element, rows, current_page);
 
@@ -741,30 +641,53 @@ popUp.addEventListener('click', function(e){
   }
 })
 
+function deactivateLefttSlider(){
+  btnLeft.setAttribute('disabled', '');
+  firstPage.setAttribute('disabled', '');
+  btnLeft.classList.add('slider-inactive');
+  btnLeft.classList.remove('slider-active');
+  firstPage.classList.add('slider-inactive');
+  firstPage.classList.remove('slider-active');
+}
+
+
+function activateLeftSlider(){
+  btnLeft.removeAttribute('disabled');
+  firstPage.removeAttribute('disabled');
+  btnLeft.classList.add('slider-active');
+  btnLeft.classList.remove('slider-inactive');
+  firstPage.classList.add('slider-active');
+  firstPage.classList.remove('slider-inactive');
+}
+
+function activateRightSlider(){
+  btnRight.removeAttribute('disabled');
+  lastPage.removeAttribute('disabled');
+  btnRight.classList.add('slider-active');
+  btnRight.classList.remove('slider-inactive');
+  lastPage.classList.add('slider-active');
+  lastPage.classList.remove('slider-inactive');
+}
+
+function deactivateRighttSlider(){
+  btnRight.setAttribute('disabled', '');
+  lastPage.setAttribute('disabled', '');
+  btnRight.classList.add('slider-inactive');
+  btnRight.classList.remove('slider-active');
+  lastPage.classList.add('slider-inactive');
+  lastPage.classList.remove('slider-active');
+
+}
 
 
 firstPage.addEventListener('click', () => {
   current_page = 0;
 
-  btnLeft.setAttribute('disabled', '');
-  firstPage.setAttribute('disabled', '');
-
-  btnLeft.classList.add('slider-inactive');
-  btnLeft.classList.remove('slider-active');
-
-  firstPage.classList.add('slider-inactive');
-  firstPage.classList.remove('slider-active');
+  deactivateLefttSlider();
 
   // ACIVATE LAST PAGES
-  btnRight.removeAttribute('disabled');
-  lastPage.removeAttribute('disabled');
 
-  btnRight.classList.add('slider-active');
-  btnRight.classList.remove('slider-inactive');
-
-  lastPage.classList.add('slider-active');
-  lastPage.classList.remove('slider-inactive');
-
+  activateRightSlider();
 
   DisplayList(pets, list_element, rows, current_page);
   currentPage.textContent = current_page + 1;
@@ -773,26 +696,11 @@ firstPage.addEventListener('click', () => {
 lastPage.addEventListener('click', () => {
   current_page = (pets.length / paginatedItems.length) - 1;
 
-  btnLeft.removeAttribute('disabled');
-  firstPage.removeAttribute('disabled');
-
-  btnLeft.classList.add('slider-active');
-  btnLeft.classList.remove('slider-inactive');
-
-  firstPage.classList.add('slider-active');
-  firstPage.classList.remove('slider-inactive');
+  activateLeftSlider();
 
   // INACTIVE LAST PAGE
-  btnRight.setAttribute('disabled', '');
-  lastPage.setAttribute('disabled', '');
 
-  btnRight.classList.add('slider-inactive');
-  btnRight.classList.remove('slider-active');
-
-  lastPage.classList.add('slider-inactive');
-  lastPage.classList.remove('slider-active');
-
-
+  deactivateRighttSlider();
 
 
   DisplayList(pets, list_element, rows, current_page);
@@ -803,31 +711,13 @@ btnRight.addEventListener('click', () => {
   current_page++;
 
   if(current_page >= (pets.length / paginatedItems.length) - 1){
-    btnRight.setAttribute('disabled', '');
-    lastPage.setAttribute('disabled', '');
-
-    btnRight.classList.add('slider-inactive');
-    btnRight.classList.remove('slider-active');
-
-    lastPage.classList.add('slider-inactive');
-    lastPage.classList.remove('slider-active');
-
-
-    // ACTIVATE LAST PAGES
-    
+    deactivateRighttSlider();
   } else {
     DisplayList(pets, list_element, rows, current_page);
   }
 
   if(current_page == 1){
-    btnLeft.removeAttribute('disabled');
-    firstPage.removeAttribute('disabled');
-
-    btnLeft.classList.add('slider-active');
-    btnLeft.classList.remove('slider-inactive');
-
-    firstPage.classList.add('slider-active');
-    firstPage.classList.remove('slider-inactive');
+    activateLeftSlider();
   }
   
   currentPage.textContent = current_page + 1;
@@ -837,27 +727,13 @@ btnRight.addEventListener('click', () => {
 btnLeft.addEventListener('click', () => {
   current_page--;
   if(current_page < 1){
-    btnLeft.setAttribute('disabled', '');
-    firstPage.setAttribute('disabled', '');
-
-    btnLeft.classList.add('slider-inactive');
-    btnLeft.classList.remove('slider-active');
-
-    firstPage.classList.add('slider-inactive');
-    firstPage.classList.remove('slider-active');
+    deactivateLefttSlider();
   } else {
     DisplayList(pets, list_element, rows, current_page);
   }
 
   if(current_page < (pets.length / paginatedItems.length) - 1){
-    btnRight.removeAttribute('disabled');
-    lastPage.removeAttribute('disabled');
-
-    btnRight.classList.add('slider-active');
-    btnRight.classList.remove('slider-inactive');
-
-    lastPage.classList.add('slider-active');
-    lastPage.classList.remove('slider-inactive');
+    activateRightSlider();
   }
   currentPage.textContent = current_page + 1;
 });
